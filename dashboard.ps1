676e23fc74f6f90afb2c9e1eaf4c0627dc69e7d7
Avri Chen-Roth <avri210984@gmail.com>
2019-09-07 22:54:27 +0300

    Update dashboard.ps1 v0.2

--- dashboard.ps1
+++ dashboard.ps1
@@ -1,15 +1,35 @@
-$NavBarLinks = @(
-    (New-UDLink -Text "UD Documentation" -Url "https://docs.universaldashboard.io" -Icon book)
-)
+<#
+Version 0.2
+#>
+Import-Module -Name $PSScriptRoot\Modules\UniversalDashboard.Helmet
+Import-Module -Name $PSScriptRoot\Modules\UniversalDashboard.Markdown
 
-$HomePage = New-UDPage -Name "Home" -Icon home -Content { 
-    New-UDCard -Title "PowerShell On Linux Website PlaceHolder" 
-} 
+$MarkdownFolder = "$PSScriptRoot\Markdown_Content"
+$AssetsFolder = Publish-UDFolder -Path "$PSScriptRoot\Assets" -RequestPath "/Assets"
+$AssetsBaseURL = $AssetsFolder.RequestPath
+$ImagesBaseURL = "$AssetsBaseURL/Images"
+$LogoImgURL = "$ImagesBaseURL/Logo_400x400.jpg"
+$LogoImg = New-UDImage -Url $LogoImgURL -Height 40 -Width 50
 
-$Page2 = New-UDPage -Name "Blog" -Icon home -Content { 
-    New-UDCard -Title "PowerShell On Linux Website PlaceHolder" 
-} 
+$EndpointInitialization = New-UDEndpointInitialization -Module @("$PSScriptRoot\Modules\UniversalDashboard.Markdown") -Variable @("MarkdownFolder",'PSScriptRoot')
+
+$Pages = @()
+$Pages += . (Join-Path -Path $PSScriptRoot -ChildPath "pages\home_page.ps1")
+$Pages += . (Join-Path -Path $PSScriptRoot -ChildPath "pages\contact_page.ps1")
+$Pages += . (Join-Path -Path $PSScriptRoot -ChildPath "pages\fav_modules_page.ps1")
+
+$Navigation = New-UDSideNav -Content {
+    New-UDSideNavItem -Text "Home" -Url "Home" -Icon Home
+    New-UDSideNavItem -Text "Favorite Modules" -Url "/FavoriteModules" -Icon Cogs 
+    New-UDSideNavItem -Text "Dev.to Blog"  -Icon blog -OnClick { Invoke-UDRedirect -Url "https://dev.to/thementor/" -OpenInNewWindow }
+    New-UDSideNavItem -Text "PowerShellOnLinux Group"  -Icon Telegram -OnClick { Invoke-UDRedirect -Url "https://t.me/PowershellOnLinux" -OpenInNewWindow }
+    New-UDSideNavItem -Text "IL PowerShell Group"  -Icon Telegram  -OnClick { Invoke-UDRedirect -Url "https://t.me/IsraelPowershellUserGroup" -OpenInNewWindow }
+    New-UDSideNavItem -Text "IL Linux User Group"  -Icon Telegram  -OnClick { Invoke-UDRedirect -Url "https://t.me/ILLUG" -OpenInNewWindow }
+    New-UDSideNavItem -Text "UD Documentation"  -Icon Book -OnClick { Invoke-UDRedirect -Url "https://docs.universaldashboard.io" -OpenInNewWindow }
+    New-UDSideNavItem -Text "Contact" -Url "/Contact" -Icon Phone 
+
+} -Fixed
 
 Start-UDDashboard -Wait -Dashboard (
-    New-UDDashboard -Title "PowerShellOnLinux" -NavbarLinks $NavBarLinks -Pages @($HomePage,$Page2)
-)
+    New-UDDashboard -Title "PowerShellOnLinux" -Navigation $Navigation -NavBarLogo $LogoImg -Pages $Pages -EndpointInitialization $EndpointInitialization
+) -PublishedFolder $AssetsFolder
